a857715c078fa4fc9a14caa96ca5601b0e4c61e2
hmcvay <hayleymcvay8@gmail.com>
2022-08-04 15:36:11 -0700

    Pull random name from api with hooks

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../index.css';
+
 
 function Contact(){
 
-  return(
-    <div>
-      <p>CONTACT</p>
-    </div>
-  )
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [contacts, setIsContacts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?inc=name,phone,email")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setIsContacts(result);
+          console.log(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+
+        <li>
+          {contacts.results[0].name.first} {contacts.results[0].name.last}
+        </li>
+    
+      </ul>
+    );
+  }
 }
 
 export default Contact;
